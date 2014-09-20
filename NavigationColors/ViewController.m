98a44c38c50b71bf3f8bcb32cbb95e25d6e6477e
spinfinite <spinfinite07@sergios-air.home>
2014-09-20 09:41:23 -0600

    Adds Changing Color Capabilities

--- NavigationColors/ViewController.m
+++ NavigationColors/ViewController.m
@@ -17,6 +17,22 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Color"
+                                                                              style:UIBarButtonItemStylePlain
+                                                                             target:self
+                                                                             action:@selector(color:)];
+}
+
+- (void)color:(UIBarButtonItem *)sender {
+    
+    self.view.backgroundColor = [UIColor whiteColor];
+
+    if(self.view.backgroundColor == [UIColor whiteColor]){
+    
+        self.view.backgroundColor = [UIColor blueColor];
+    }
+    else self.view.backgroundColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning {
